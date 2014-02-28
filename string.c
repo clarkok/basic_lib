cebb0297026a2a501eb22298e53ecffc03abeb7e
Clarkok <mail@clarkok.com>
2014-02-28 23:49:30 +0800

    completed comments

--- string.c
+++ string.c
@@ -3,6 +3,11 @@
 #include "string.h"
 #include "stdlib.h"
 
+/**
+ * To calc the highist one in x, and stored the index in res
+ * @param int x the number to calc
+ * @param u32 res the result
+ */
 #define highist_one( x, res )   \
     {                           \
         __asm mov ebx, x        \
@@ -10,6 +15,10 @@
         __asm mov res, eax      \
     }
 
+/**
+ * The constructer of String
+ * @return String the new String
+ */
 String *string () {
     String *s = malloc_string();
     s->_data = malloc( INIT_LENGTH );
@@ -22,6 +31,11 @@ String *string () {
     return s;
 }
 
+/**
+ * The constructer of String from c-style string
+ * @paran char *p the c-style string
+ * @return String the new String
+ */
 String *string_from_chars ( const char *p ) {
     String *s = malloc_string();
     u32 len = strlen( p );
@@ -41,6 +55,11 @@ String *string_from_chars ( const char *p ) {
     return s;
 }
 
+/**
+ * The constructer of String from an existing String
+ * @param String *l the String
+ * @return String the new String
+ */
 String *string_from_string ( const String *l ) {
     String *s = malloc_string();
 
@@ -53,11 +72,21 @@ String *string_from_string ( const String *l ) {
     return s;
 }
 
+/**
+ * The destructer of String
+ * @param String *s the String to be destroyed
+ */
 void _string( String *s ) {
     free( s->_data );
     free( s );
 }
 
+/**
+ * To expand the allocated memory to n, if n is larger than current capacity of the String
+ * @param String *s the String under modifying
+ * @param u32 n the new capacity
+ * @return String the original String but after modifying
+ */
 String *string_reserve ( String *s, u32 n ) {
     int capacity;
     if ( n > s->capacity ) {
@@ -69,6 +98,11 @@ String *string_reserve ( String *s, u32 n ) {
     return s;
 }
 
+/**
+ * To clear a String, which means to make the length of the String 0
+ * @param String *s the String under modifying
+ * @return String the original String but after modifying
+ */
 String *string_clear ( String *s ) {
     s->length = 0;
     *(s->_data) = 0;
@@ -76,6 +110,12 @@ String *string_clear ( String *s ) {
     return s;
 }
 
+/**
+ * To append one String to another
+ * @param String *s the String to be appended to
+ * @param String *a the String to be appended to s
+ * @return String the original String but after modifying
+ */
 String *string_append_string ( String *s, const String *a ) {
     u32 length = (s->length + a->length);
     u32 capacity = length + 1;
@@ -93,6 +133,12 @@ String *string_append_string ( String *s, const String *a ) {
     return s;
 }
 
+/**
+ * To append one c-style to a String
+ * @param String *s the String to be appended to
+ * @param char *a the c-style string to be appended to s
+ * @return String the original String but after modifying
+ */
 String *string_append_chars ( String *s, const char *a ) {
     u32 length = s->length + strlen( a );
     u32 capacity = length + 1;
@@ -110,6 +156,12 @@ String *string_append_chars ( String *s, const char *a ) {
     return s;
 }
 
+/**
+ * To push a character in the end of a String
+ * @param String *s the String to be pushed in
+ * @param char a the character to be pushed
+ * @return String the original String but after modifying
+ */
 String *string_push_back ( String *s, const char a ) {
     char *p;
     if ( ++(s->length) >= (s->capacity)) {
@@ -123,6 +175,13 @@ String *string_push_back ( String *s, const char a ) {
     return s;
 }
 
+/**
+ * To assign a String's content to another, but the two String will not be connected.
+ * Each of them has it own buffer
+ * @param String *s the String to be re-assigned
+ * @param String *a the String to give its content
+ * @return String the original String but after modifying
+ */
 String *string_assign_string ( String *s, const String *a ) {
     u32 capacity = a->length + 1;
 
@@ -138,6 +197,13 @@ String *string_assign_string ( String *s, const String *a ) {
     return s;
 }
 
+/**
+ * To assign a c-style string's content to a String, but the two String will not be connected.
+ * Each of them has it own buffer
+ * @param String *s the String to be re-assigned
+ * @param char *a the String to give its content
+ * @return String the original String but after modifying
+ */
 String *string_assign_chars ( String *s, const char *a ) {
     u32 len = strlen( a );
     u32 capacity = len + 1;
@@ -158,6 +224,13 @@ String *string_assign_chars ( String *s, const char *a ) {
     return s;
 }
 
+/**
+ * To insert a String into another
+ * @param String *s the String to be inserted to
+ * @param u32 pos the position the insertion will append at
+ * @param String *i the String to be inserted with
+ * @return String the original String but after modifying
+ */
 String *string_insert_string ( String *s, u32 pos, const String *i ) {
     u32 length = s->length + i->length;
     u32 capacity = length + 1;
@@ -180,6 +253,13 @@ String *string_insert_string ( String *s, u32 pos, const String *i ) {
     return s;
 }
 
+/**
+ * To insert a c-style string into a String
+ * @param String *s the String to be inserted to
+ * @param u32 pos the position the insertion will append at
+ * @param char *i the String to be inserted with
+ * @return String the original String but after modifying
+ */
 String *string_insert_chars ( String *s, u32 pos, const char *i ) {
     u32 il = strlen(i);
     u32 length = s->length + il;
@@ -203,6 +283,13 @@ String *string_insert_chars ( String *s, u32 pos, const char *i ) {
     return s;
 }
 
+/**
+ * To erase a substring in a String
+ * @param String *s the String to deal with
+ * @param u32 pos the beginning position of the erasing
+ * @param u32 len the length of the erased substring
+ * @return String the original String but after modifying
+ */
 String *string_erase ( String *s, u32 pos, u32 len ) {
     if ( !len || pos + len > s->length ) {
         s->_data[pos] = 0;
@@ -214,6 +301,14 @@ String *string_erase ( String *s, u32 pos, u32 len ) {
     return s;
 }
 
+/**
+ * To replace a substring in a String with another String
+ * @param String *s the container of the substring to be replaced
+ * @paran u32 pos the beginning position of the substring
+ * @param u32 len the length of the substring
+ * @param String *r the new String to replace the substring
+ * @return String the original String but after modifying
+ */
 String *string_replace_string ( String *s, u32 pos, u32 len, const String *r ) {
     int capacity = s->length - len + r->length + 1;
 
@@ -239,6 +334,14 @@ String *string_replace_string ( String *s, u32 pos, u32 len, const String *r ) {
     return s;
 }
 
+/**
+ * To replace a substring in a String with a c-style string
+ * @param String *s the container of the substring to be replaced
+ * @paran u32 pos the beginning position of the substring
+ * @param u32 len the length of the substring
+ * @param char *r the new c-style string to replace the substring
+ * @return String the original String but after modifying
+ */
 String *string_replace_chars ( String *s, u32 pos, u32 len, const char *r ) {
     int rl = strlen(r);
     int capacity = s->length - len + rl + 1;
@@ -265,6 +368,12 @@ String *string_replace_chars ( String *s, u32 pos, u32 len, const char *r ) {
     return s;
 }
 
+/**
+ * To swap the two String
+ * @param String *s one of them
+ * @param String *p the other of them
+ * @return String *p
+ */
 String *string_swap ( String *s, String *p ) {
     String t = *s;
     *s = *p;
@@ -272,10 +381,22 @@ String *string_swap ( String *s, String *p ) {
     return s;
 }
 
+/**
+ * To get the c-style string of a String
+ * @param String* the String
+ * @return char* the c-style string
+ */
 const char *string_c_str ( String *s ) {
     return s->_data;
 }
 
+/**
+ * To copy a substring of a String
+ * @param String *s the original String
+ * @param u32 len the length of the substring, NOTE: I know it is strange, but it is same with C++
+ * @param u32 pos the beginning position of the substring
+ * @return String* the copied String
+ */
 String *string_copy ( String *s, u32 len, u32 pos ) {
     String *ret;
 
@@ -283,7 +404,7 @@ String *string_copy ( String *s, u32 len, u32 pos ) {
         return NULL;
     }
 
-    ret = string_reserve( string_from_chars(""), len + 1 );
+    ret = string_reserve( string(), len + 1 );
 
     if ( pos + len > s->length) {
         len = s->length - pos;
@@ -296,10 +417,26 @@ String *string_copy ( String *s, u32 len, u32 pos ) {
     return ret;
 }
 
+/**
+ * Find String in a String
+ * @param String *s the String to be searched
+ * @param String *str the target String
+ * @param u32 pos the first position to search
+ * @param u32 n the max length of the Strings to compare
+ * @return u32 the index of the found String, if none is found, -1 is returned
+ */
 u32 string_find_string ( String *s, const String *str, u32 pos, u32 n ) {
     return string_find_chars( s, str->_data, pos, n );
 }
 
+/**
+ * Find c-style string in a String
+ * @param String *s the String to be searched
+ * @param char *str the target String
+ * @param u32 pos the first position to search
+ * @param u32 n the max length of the Strings to compare
+ * @return u32 the index of the found String, if none is found, -1 is returned
+ */
 u32 string_find_chars ( String *s, const char *str, u32 pos, u32 n ) {
     u32 i;
     u32 l = s->length - n;
@@ -311,6 +448,13 @@ u32 string_find_chars ( String *s, const char *str, u32 pos, u32 n ) {
     return -1;
 }
 
+/**
+ * Find a character in a String
+ * @param String *s the String to be searched
+ * @param char c the target character 
+ * @param u32 pos the first position to be searched
+ * @return u32 the index of the found String, if none is found, -1 is returned
+ */
 u32 string_find_char ( String *s, char c, u32 pos ) {
     char *p = s->_data + pos;
     char *end = s->_data + s->length;
@@ -327,10 +471,26 @@ u32 string_find_char ( String *s, char c, u32 pos ) {
     return -1;
 }
 
+/**
+ * Find String in a String, reversely
+ * @param String *s the String to be searched
+ * @param String *str the target String
+ * @param u32 pos the first position to search
+ * @param u32 n the max length of the Strings to compare
+ * @return u32 the index of the found String, if none is found, -1 is returned
+ */
 u32 string_rfind_string ( String *s, const String *str, u32 pos, u32 n ) {
     return string_rfind_chars( s, str->_data, pos, n );
 }
 
+/**
+ * Find c-style string in a String, reversely
+ * @param String *s the String to be searched
+ * @param char *str the target String
+ * @param u32 pos the first position to search
+ * @param u32 n the max length of the Strings to compare
+ * @return u32 the index of the found String, if none is found, -1 is returned
+ */
 u32 string_rfind_chars ( String *s, const char *str, u32 pos, u32 n ) {
     u32 i;
 
@@ -341,6 +501,13 @@ u32 string_rfind_chars ( String *s, const char *str, u32 pos, u32 n ) {
     return ( string_compare_chars(s, 0, n, str)==0 ? 0 : -1 );
 }
 
+/**
+ * Find a character in a String, reversely
+ * @param String *s the String to be searched
+ * @param char c the target character 
+ * @param u32 pos the first position to be searched
+ * @return u32 the index of the found String, if none is found, -1 is returned
+ */
 u32 string_rfind_char ( String *s, char c, u32 pos ) {
     char *p = s->_data + pos;
     char *begin = s->_data;
@@ -357,14 +524,37 @@ u32 string_rfind_char ( String *s, char c, u32 pos ) {
     return ( *p==c ? 0 : -1);
 }
 
+/**
+ * Compare two String
+ * @param String *s one of them
+ * @param u32 pos the beginning position of the comparation
+ * @param u32 len the max length to be compared, when set s->length + 1, the method will work as C++ s.compare(c)
+ * @param String *c the other of them
+ * @return if positive, then s > c, if negetive, the s < c, if zero, the s = c
+ */
 int string_compare_string ( String *s, u32 pos, u32 len, const String *c ) {
     return strncmp( s->_data + pos, c->_data, len );
 }
 
+/**
+ * Compare a String and a c-style string
+ * @param String *s one of them
+ * @param u32 pos the beginning position of the comparation
+ * @param u32 len the max length to be compared, when set s->length + 1, the method will work as C++ s.compare(c)
+ * @param char *c the other of them
+ * @return if positive, then s > c, if negetive, the s < c, if zero, the s = c
+ */
 int string_compare_chars ( String *s, u32 pos, u32 len, const char *c ) {
     return strncmp( s->_data + pos, c, len );
 }
 
+/**
+ * Get the substring of a String, almost same as copy but with different arguments order
+ * @param String *s the original String
+ * @param u32 pos the beginning position of the substring
+ * @param u32 len the length of the substring, NOTE: I know it is strange, but it is same with C++
+ * @return String* the copied String
+ */
 String *string_substr ( String *s, u32 pos, u32 len ) {
     return string_copy( s, len, pos );
 }
